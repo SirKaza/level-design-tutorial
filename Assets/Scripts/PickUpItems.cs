573df781cf4d2e895c894dad525b8eb06f178b8d
Joan Martí Olivart <jmartiolivart@gmail.com>
2025-01-27 15:14:07 +0100

    added press "e" to get item
    In the PickUpItems added a Getkey condition

--- Assets/Scripts/PickUpItems.cs
+++ Assets/Scripts/PickUpItems.cs
@@ -2,9 +2,27 @@ using UnityEngine;
 
 public class PickUpItems : MonoBehaviour
 {
+    private bool playerInRange = false;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
             Destroy(gameObject);
         }
